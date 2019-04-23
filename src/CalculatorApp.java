c9cf02fa2f5acb1de517123f629655dcee3fb263
JayDarrah <jasondarrah@gmail.com>
2019-04-23 09:30:41 -0400

    Just a quick update

--- src/CalculatorApp.java
+++ src/CalculatorApp.java
@@ -1,6 +1,6 @@
 /*
  * Chapter 2: Calculator Homework
- * Author: Jason Darrah
+ * Author: J. Darrah
  * Date: 04-22-2019
  * Purpose: Perform arithmetic expressions on two int variables. Repeat for double variables
  * 			print output to screen.
@@ -8,12 +8,13 @@
 public class CalculatorApp {
 
 	public static void main(String[] args) {
+		// define 2 ints, define sum, diff, product, quotient. Repeat for doubles.
 		int x = 7, y = 5;
 		int xysum=x+y, xydiff=x-y, xyprod=x*y, xyquot=x/y; 
 		double a = 13.19, b = 6.11;
 		double absum=a+b, abdiff=a-b, abprod=a*b, abquot=a/b;
 		
-		System.out.println("Chapter 2 Calculator v.1.0 By Jason Darrah\n");
+		System.out.println("Chapter 2 Calculator v.1.0 By J. Darrah\n");
 		System.out.println("Integer values x="+x+", y="+y);
 		System.out.println("x+y = "+xysum);
 		System.out.println("x-y = "+xydiff);
